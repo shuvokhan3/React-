45243f5a352e62f9bac2bb635cad31bb8519ea1f
shuvokhan3 <shuvoj360@gmail.com>
2025-10-22 13:07:44 +0600

    practice jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,9 +1,25 @@
+import { Fragment } from "react";
 
 //Hero component 
 const hero = () => {
     return (
+        //jsx
+        //Opening and closing tag must need in jsx
         <div>
-            <p>This is Hero section</p>
+            //Those are the main 
+
+            //react attribure must write in camelcase
+
+            //inside jsx it possible to implement js directly
+            <button onClick={()=>{alert("Hello")}}>Click</button>
+
+            // In react if i want to write enline this time i show write like this
+            <p style={
+                {color: 'red'}
+            }>text</p>
+
+            //All tag self close in jsx
+            <br/>
         </div>
     );
 };
