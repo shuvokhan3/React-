c0a574c769ad142f9f4dfeefeda852faf6306736
shuvokhan3 <shuvoj360@gmail.com>
2025-10-22 13:27:21 +0600

    Some chage on practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from "./component/header";
 import Footer from "./component/footer";
 import Hero from "./component/hero"
-
+import Ifelse from './component/ifelse'
 const App = () => {
   return (
     <div>
-      <Header/>
-       <Hero />
-      <Footer/>
+      <Ifelse/>
     </div>
   );
 };
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -6,19 +6,16 @@ const hero = () => {
         //jsx
         //Opening and closing tag must need in jsx
         <div>
-            //Those are the main 
-
-            //react attribure must write in camelcase
-
-            //inside jsx it possible to implement js directly
+        
+         
             <button onClick={()=>{alert("Hello")}}>Click</button>
 
-            // In react if i want to write enline this time i show write like this
+           
             <p style={
                 {color: 'red'}
             }>text</p>
 
-            //All tag self close in jsx
+           
             <br/>
         </div>
     );
